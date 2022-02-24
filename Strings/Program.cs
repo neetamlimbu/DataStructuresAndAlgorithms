b9096321e6e060cf52e938778f7e0037361132aa
neetam limbu <neetam.limbu@hkbn.com.hk>
2022-02-24 12:35:18 +0545

    InitialCommit

---
+++ Strings/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Removing duplicate characters from string...");
+
+            //null or empty case
+            string testCase1 = null;
+            string testCase2 = "";
+
+            if(string.IsNullOrEmpty(testCase1))
+            {
+                Console.WriteLine("String is null");
+            }
+
+            if(string.IsNullOrEmpty(testCase2))
+            {
+                Console.WriteLine("String is empty");
+            }
+
+            //sample test  1
+            string testCase3 = "Google";
+
+            ArrayList duplicateList = new ArrayList();
+            StringBuilder sb = new StringBuilder();
+
+            foreach(char c in testCase3)
+            {
+                if(!duplicateList.Contains(c))
+                {
+                    duplicateList.Add(c);
+                    sb.Append(c);
+                }
+            }
+
+            Console.WriteLine(sb);
+
+        }
+    }
+}
