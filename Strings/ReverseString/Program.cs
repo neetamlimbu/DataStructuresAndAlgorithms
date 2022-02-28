9a5354b31eb19f97d84fdf20b0f9d918f2715bf7
neetam limbu <neetam.limbu@hkbn.com.hk>
2022-02-28 11:21:21 +0545

    Reverse String Initial Commit

---
+++ Strings/ReverseString/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reverse String...");
+
+            //Test Case 1
+            string testCase1 = "hello world";
+            string testCase2 = "";
+            string testCase3 = null;
+            
+            Console.WriteLine(Reverse(testCase1));
+            Console.WriteLine(Reverse(testCase2));
+            Console.WriteLine(Reverse(testCase3));
+
+        }
+
+        static string Reverse(string str)
+        {
+            if(string.IsNullOrEmpty(str))
+                return str;
+
+            StringBuilder reverseString = new StringBuilder();
+            for(int i=str.Length -1; i >= 0; i--)
+            {
+                reverseString.Append(str[i]);
+            }
+
+            return reverseString.ToString();
+        }
+    }
+}
