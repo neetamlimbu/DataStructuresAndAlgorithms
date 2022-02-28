33eab878476e79e2b3b1171653bac502f0f411f0
neetam limbu <neetam.limbu@hkbn.com.hk>
2022-02-28 14:35:28 +0545

    Initial Commit

---
+++ Strings/RemoveDuplicateCharacters/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace RemoveDuplicateCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Remove Duplicate Characters...");
+
+            string[] testCases = 
+            {
+                "different",
+                "characters",
+                "    ",
+                null
+            };
+
+            foreach(string testCase in testCases)
+            {
+                Console.WriteLine("{0} after removing duplicate characters => {1}" , testCase, RemoveDuplicates(testCase));
+            }
+        }
+
+        static string RemoveDuplicates(string s)
+        {
+            if(string.IsNullOrWhiteSpace(s))
+                return s;
+
+            ArrayList characters = new ArrayList();
+            StringBuilder result = new StringBuilder();
+
+            foreach(Char c in s)
+            {
+                if(!characters.Contains(c))
+                {
+                    characters.Add(c);
+                    result.Append(c);
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
