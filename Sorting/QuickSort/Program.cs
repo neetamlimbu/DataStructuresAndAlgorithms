33d60f9ff5b1208a00659f6fcb3c42a6e3e98264
neetam limbu <neetam.limbu@hkbn.com.hk>
2022-03-05 20:51:22 +0545

    Added comments

--- Sorting/QuickSort/Program.cs
+++ Sorting/QuickSort/Program.cs
@@ -16,26 +16,31 @@ namespace QuickSort
 
         static public int Partition(int[] input, int lowerBound, int upperBound)
         {
-            int pivot = input[lowerBound];
-            int start = lowerBound;
-            int end = upperBound;
+            int pivot = input[lowerBound]; //Pivot element to comapre with, in this case input[0] that is 7
+            int start = lowerBound;        //Pointer to the start of the left sub array
+            int end = upperBound;          //Pointer to the end of the right sub array
 
-            while (start < end)
+            while (start < end)  //Repeat the process of swapping until the left sub array pointer is greater than or equal to right sub array pointer
             {
                 while (start <= input.Length - 1 && input[start] <= pivot)
                 {
-                    start++;
+                    start++; //Increment left pointer until the pivot element is less that the element at index "input[start]"
                 }
                 while (end >= 0 && input[end] > pivot)
                 {
-                    end--;
+                    end--; //Decrement right pointer until the pivot element is greater than the element at index "input[end]"
                 }
+                //Swap only when the left pointer is less than end pointer
+                //Because the left sub array is less than the pivot element and the right sub array is greater than the pivot element
+                //Swap greater elements to the right and lesser elements to the left
                 if (start < end)
                 {
                     Swap(input, start, end);
                 }
             }
 
+            //If left sub array pointer is greater than right sub array pointer, then it means more elements less than pivot element exists in right sub array
+            //So in this case, pivot element is swapped with lesser element at index of right sub array pointer
             Swap(input, end, lowerBound);
 
             return end;
