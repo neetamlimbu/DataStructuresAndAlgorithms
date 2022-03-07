6af675a8be6f716d76529ef39f202e2c3ebfcc72
neetam limbu <neetam.limbu@hkbn.com.hk>
2022-03-07 18:50:59 +0545

    Initial Commit

---
+++ Numbers/ReturnRemainder/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ReturnRemainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Return Remainder...");
+            // Console.WriteLine(ReturnRemainder(20, 0));
+            // Console.WriteLine(ReturnRemainder(5, 8));
+            Console.WriteLine(ReturnRemainder(10, 2));
+            Console.WriteLine(ReturnRemainder(11, 2));
+        }
+
+        static int ReturnRemainder(int x, int y)
+        {
+            if (y == 0)
+            {
+                throw new Exception(string.Format("Cannot divide {0} by zero.", x));
+            }
+            else if (x < y)
+            {
+                throw new Exception(string.Format("Number being divided {0} cannot be less than divisor {1}.", x, y));
+            }
+            else
+            {
+                return (x % y);
+            }
+        }
+    }
+}
---
+++ Numbers/SwapTwoNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SwapTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Swap two numbers...");
+
+            int a = 52; //First Number
+            int b = 37; //Second Number
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+            Console.WriteLine("First number is {0} and second number is {1}", a, b);
+            Console.WriteLine("After swapping => first number is {0} and second number is {1}", a, b);
+        }
+    }
+}
