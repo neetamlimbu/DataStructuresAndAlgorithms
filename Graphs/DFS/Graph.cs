a0c9d9b97cdd5f529ea20d5b415d78153cb23b78
neetam limbu <neetam.limbu@hkbn.com.hk>
2022-03-08 22:34:20 +0545

    Updated comments

--- Graphs/DFS/Graph.cs
+++ Graphs/DFS/Graph.cs
@@ -9,29 +9,30 @@ namespace DFS
         private int v; //No. of vertices
         private List<int>[] adj; //Adjacency list of array of int
 
-        public Graph(int vertices)
+        public Graph(int noOfVertices)
         {
-            v = vertices;
-            adj = new List<int>[vertices];
-            for (int i = 0; i < vertices; i++)
+            v = noOfVertices;
+            adj = new List<int>[noOfVertices];
+
+            for (int i = 0; i < noOfVertices; i++)
             {
                 adj[i] = new List<int>();
             }
         }
 
-        public void AddEdge(int v, int w)
+        public void AddEdge(int vertex, int weight)
         {
-            adj[v].Add(w); //Add w to v's list
+            adj[vertex].Add(weight); //Add weight to vertex list
         }
 
-        private void VisitNode(int v, bool[] visited)
+        private void VisitNode(int startVertex, bool[] visited)
         {
             // Mark the current node as visited and print it
-            visited[v] = true;
-            Console.Write(v + " ");
+            visited[startVertex] = true;
+            Console.Write(startVertex + " ");
 
             // Recur for all the vertices adjacent to this vertex
-            foreach (int vertex in adj[v])
+            foreach (int vertex in adj[startVertex])
             {
                 if (!visited[vertex])
                 {
@@ -40,13 +41,13 @@ namespace DFS
             }
         }
 
-        public void DFS(int vertex)
+        public void DFS(int startVertex)
         {
             bool[] visited = new bool[v];
 
             // Call the recursive helper function to print DFS traversal starting from all vertices one by one
             // Recursion calls will act like a stack ideal for DFS
-            VisitNode(vertex, visited);
+            VisitNode(startVertex, visited);
         }
     }
 }
