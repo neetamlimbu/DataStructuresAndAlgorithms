d8906af1e2256cd6a6ea6ae5b083ccda43a4ff4d
neetam limbu <neetam.limbu@hkbn.com.hk>
2022-03-10 08:32:59 +0545

    Initial Commit

---
+++ Numbers/SumOfTwoNumbers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace SumOfTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 3, 2, 3, 4, 8, 5, 7 };
+            Array.ForEach(TwoSum(arr, 9), element => Console.Write(element + " "));
+        }
+
+        //Index of two numbers to a given target
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> map = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int firstNumber = nums[i];
+                int requiredNumber = target - firstNumber;
+
+                if (map.ContainsKey(requiredNumber))
+                {
+                    int[] result = { map[requiredNumber], i };
+                    return result;
+                }
+
+                if (!map.ContainsKey(firstNumber))
+                {
+                    map.Add(firstNumber, i);
+                }
+            }
+            return null;
+        }
+    }
+}
