2d75095b9e0aa68441a0f75ff7cdd8c10f494b6d
neetam limbu <neetam.limbu@hkbn.com.hk>
2022-03-19 11:11:20 +0545

    Initial Commit

--- Trees/SortedArrayToBST/Program.cs
+++ Trees/SortedArrayToBST/Program.cs
@@ -6,7 +6,35 @@ namespace SortedArrayToBST
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Sorted Array to Binary Search Tree...");
+
+            int[] arr = { 3, 9, 20, 15, 7 };
+            PreOrderTraversal(SortedArrayToBST(arr, 0, arr.Length - 1));
+        }
+
+        static void PreOrderTraversal(TreeNode treeNode)
+        {
+            Console.Write(treeNode.val + " ");
+            if (treeNode.left != null)
+                PreOrderTraversal(treeNode.left);
+
+            if (treeNode.right != null)
+                PreOrderTraversal(treeNode.right);
+        }
+
+
+        static TreeNode SortedArrayToBST(int[] input, int start, int end)
+        {
+            if (start > end)
+                return null;
+
+            int mid = (start + end) / 2;
+
+            TreeNode treeNode = new TreeNode(input[mid]);
+            treeNode.left = SortedArrayToBST(input, start, mid - 1);
+            treeNode.right = SortedArrayToBST(input, mid + 1, end);
+
+            return treeNode;
         }
     }
 }
