a1594f903c4066cdc2d185c534edfd1f1254984a
neetam limbu <neetam.limbu@hkbn.com.hk>
2022-03-19 13:46:13 +0545

    Initial Commit

---
+++ Trees/postordertraversal/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace postordertraversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Preorder Traversal - LeetCode 145");
+
+            TreeNode root = new TreeNode(1); //[1,null,2,3]
+            root.right = new TreeNode(2);
+            root.right.left = new TreeNode(3);
+
+            IList<int> list = PostorderTraversal(root);
+            Console.Write("[ ");
+            foreach (int i in list)
+            {
+                Console.Write(i + " ");
+            }
+            Console.Write("]");
+            Console.WriteLine();
+
+            list = PostorderTraversal(null);
+            Console.Write("[ ");
+            foreach (int i in list)
+            {
+                Console.Write(i + " ");
+            }
+            Console.Write("]");
+            Console.WriteLine();
+        }
+
+        static IList<int> PostorderTraversal(TreeNode root)
+        {
+            return PostorderTraversal(root, new List<int>());
+        }
+
+        static IList<int> PostorderTraversal(TreeNode treeNode, IList<int> list)
+        {
+            if (treeNode == null)
+                return list;
+
+            PostorderTraversal(treeNode.left, list);
+            PostorderTraversal(treeNode.right, list);
+            list.Add(treeNode.value);
+
+            return list;
+        }
+
+
+    }
+}
