79ab6abaafaf668ce33ee691d97eab57c67e7957
neetam limbu <neetam.limbu@hkbn.com.hk>
2022-04-04 00:07:19 +0545

    Initial Commit

---
+++ Strings/MinimumIndexSum/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace MinimumIndexSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Minimum Index Sum of Two Lists - LeetCode 599");
+
+            string[] list1 = { "Shogun", "Tapioca Express", "Burger King", "KFC" };
+            string[] list2 = { "KFC", "Shogun", "Burger King" };
+
+            string[] result = FindRestaurantBruteForce(list1, list2);
+            Console.WriteLine("Using Brute Force...");
+            Array.ForEach(result, Console.WriteLine);
+
+            result = FindRestaurantUsingDictionary(list1, list2);
+            Console.WriteLine("Using Dictionary...");
+            Array.ForEach(result, Console.WriteLine);
+        }
+
+        static string[] FindRestaurantBruteForce(string[] list1, string[] list2)
+        {
+            ArrayList result = new ArrayList();
+            int minIndex = int.MaxValue;
+
+            for (int i = 0; i < list1.Length; i++)
+            {
+                for (int j = 0; j < list2.Length; j++)
+                {
+                    if (list1[i] == list2[j])
+                    {
+                        if (minIndex > (i + j))
+                        {
+                            minIndex = i + j;
+                            result.Clear();
+                            result.Add(list1[i]);
+                        }
+                        else if (minIndex == (i + j))
+                        {
+                            result.Add(list1[i]);
+                        }
+                    }
+                }
+            }
+
+            return (string[])result.ToArray(typeof(string));
+        }
+
+
+        static string[] FindRestaurantUsingDictionary(string[] list1, string[] list2)
+        {
+            Dictionary<string, int> resList = new Dictionary<string, int>();
+
+            int minIndex = int.MaxValue;
+            for (int i = 0; i < list1.Length; i++)
+            {
+                if (!resList.ContainsKey(list1[i]))
+                {
+                    resList.Add(list1[i], i);
+                }
+            }
+
+            ArrayList result = new ArrayList();
+            for (int i = 0; i < list2.Length; i++)
+            {
+                if (resList.ContainsKey(list2[i]))
+                {
+                    int index = resList[list2[i]];
+                    if (minIndex > (index + i))
+                    {
+                        minIndex = index + i;
+                        result.Clear();
+                        result.Add(list2[i]);
+                    }
+                    else if (minIndex == (index + i))
+                    {
+                        result.Add(list2[i]);
+                    }
+                }
+            }
+
+            return (string[])result.ToArray(typeof(string));
+        }
+
+    }
+}
---
+++ Strings/NumberOfLines/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NumberOfLines
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Number of Lines To Write String - LeetCode 806");
+
+            int[] widths = { 4, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 };
+            string s = "bbbcccdddaaa";
+            int[] result = NumberOfLines(widths, s);
+            Console.WriteLine(result[0]);
+            Console.WriteLine(result[1]);
+        }
+
+        static int[] NumberOfLines(int[] widths, string s)
+        {
+            string[] chars = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z".Split(",");
+
+            Dictionary<string, int> charMap = new Dictionary<string, int>();
+            for (int i = 0; i < widths.Length; i++)
+            {
+                charMap.Add(chars[i], widths[i]);
+            }
+
+            int[] result = new int[2];
+            int lines = 1;
+            int totalPixels = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                int pixels = charMap[s[i].ToString()];
+
+                if ((totalPixels + pixels) > 100)
+                {
+                    lines++;
+                    totalPixels = pixels;
+                }
+                else
+                {
+                    totalPixels += pixels;
+                }
+            }
+
+            result[0] = lines;
+            result[1] = totalPixels;
+            return result;
+        }
+    }
+}
