056cfa7f36acbe1c6b1b792ae28aee7c7f6bf33c
neetam limbu <neetam.limbu@hkbn.com.hk>
2022-04-04 19:26:05 +0545

    Initial Commit

---
+++ LinkedLists/MiddleOfLinkedList/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace MiddleOfLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Middle of the Linked List - LeetCode 876");
+
+            ListNode head = new ListNode(1);
+            head.next = new ListNode(2);
+            head.next.next = new ListNode(3);
+            head.next.next.next = new ListNode(4);
+            head.next.next.next.next = new ListNode(5);
+
+            ListNode middle = MiddleNode(head);
+            while(middle != null)
+            {
+                Console.Write(middle.val + " ");
+                middle = middle.next;
+            }
+        }
+
+        static ListNode MiddleNode(ListNode head)
+        {
+
+            if (head == null)
+                return null;
+
+            ListNode slow = head;
+            ListNode fast = head;
+
+            while (fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            return slow;
+        }
+    }
+}
