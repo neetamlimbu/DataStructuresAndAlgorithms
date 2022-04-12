b70912f43848ec8d919344ddaf718ed08aa28d67
neetam limbu <neetam.limbu@hkbn.com.hk>
2022-04-12 06:37:47 +0545

    Initial Commit

---
+++ Arrays/ContainsDuplicate/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContainsDuplicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Contains Duplicate - LeetCode 217");
+
+            // int[] nums = { 3, 3 };
+            int[] nums = { 1, 2, 3, 1 };
+            Console.WriteLine(ContainsDuplicate(nums));
+        }
+
+        static bool ContainsDuplicate(int[] nums)
+        {
+            if (nums.Length <= 1)
+                return false;
+
+            HashSet<int> list = new HashSet<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (list.Contains(nums[i]))
+                    return true;
+                else
+                    list.Add(nums[i]);
+            }
+
+            return false;
+        }
+    }
+}
