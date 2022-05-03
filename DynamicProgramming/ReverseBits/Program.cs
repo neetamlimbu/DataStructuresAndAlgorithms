52b92606c9400444aeced8b373c5953c662aaeda
neetam limbu <neetam.limbu@hkbn.com.hk>
2022-05-03 22:33:33 +0545

    Initial Commit

---
+++ DynamicProgramming/ReverseBits/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ReverseBits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reverse Bits - LeetCode 190");
+
+            Console.WriteLine(reverseBits(4));
+        }
+
+        static uint reverseBits(uint n)
+        {
+            uint result = 0;
+            for (int i = 0; i < 32; i++,  n >>= 1)
+            {
+                result = (result << 1) + (n & 1);
+            }
+
+            return result;
+        }
+    }
+}
